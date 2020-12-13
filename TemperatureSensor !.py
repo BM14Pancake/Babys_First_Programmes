10194deb0909bfd29a6ad573aeaaf1a3915fabc9
Pancake <75526428+BM14Pancake@users.noreply.github.com>
2020-12-13 21:02:01 +0800

    Update TemperatureSensor !.py

--- TemperatureSensor !.py
+++ TemperatureSensor !.py
@@ -1,3 +1,5 @@
+#Programme that updates average temperature with each value added, up to 24 temperature readings 04/12/20
+
 temp_list= []
 total=float(0)
 
@@ -5,7 +7,6 @@ while len(temp_list)<24:
     newtemp=float(input("Enter the room temperature for this interval: "))
     temp_list.append(newtemp)
     total=sum(temp_list)
-    print(len(temp_list))
     average=total/(len(temp_list))
     print("Current average temperature for the day is: {:.2f} degrees Celsius.".format(average))
 
